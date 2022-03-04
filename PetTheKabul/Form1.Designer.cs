aa216d497630d5e64200df7dedfdd674b4c5f907
Luxtsii <venndatio@gmail.com>
2022-03-04 19:52:32 +0000

    Add project files.

---
+++ PetTheKabul/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace PetTheKabul
+{
+    partial class PetTheKabul
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PetTheKabul));
+            this.kabulBox = new System.Windows.Forms.PictureBox();
+            this.kabulTray = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.kabulBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kabulBox
+            // 
+            this.kabulBox.Location = new System.Drawing.Point(0, 0);
+            this.kabulBox.Margin = new System.Windows.Forms.Padding(0);
+            this.kabulBox.Name = "kabulBox";
+            this.kabulBox.Size = new System.Drawing.Size(96, 96);
+            this.kabulBox.TabIndex = 0;
+            this.kabulBox.TabStop = false;
+            this.kabulBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.kabulBox_MouseDown);
+            this.kabulBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.kabulBox_MouseUp);
+            // 
+            // kabulTray
+            // 
+            this.kabulTray.BalloonTipText = "Pet him.";
+            this.kabulTray.BalloonTipTitle = "Pet the Kabul.";
+            this.kabulTray.Icon = ((System.Drawing.Icon)(resources.GetObject("kabulTray.Icon")));
+            this.kabulTray.Text = "Kabul";
+            this.kabulTray.Visible = true;
+            // 
+            // PetTheKabul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(179, 165);
+            this.Controls.Add(this.kabulBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PetTheKabul";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Pet the Kabul";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kabulBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox kabulBox;
+        private System.Windows.Forms.NotifyIcon kabulTray;
+    }
+}
+
---
+++ PetTheKabul/Form1.cs
@@ -0,0 +1,81 @@
+﻿using PetTheKabul.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetTheKabul
+{
+    public partial class PetTheKabul : Form
+    {
+        public static Image kabulIdle;
+        public static Image kabulPet;
+
+        public static ContextMenu ctx;
+        public static MenuItem exit;
+
+
+        public PetTheKabul()
+        {
+            InitializeComponent();
+
+            // Changes form size to be that of the picture. 
+            this.Size = new Size(kabulBox.Width, kabulBox.Height);
+
+            // Gets screen area and moves window to appropriate corner.
+            Rectangle screenArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(screenArea.Right, screenArea.Bottom - Size.Height);
+
+            // Some stuff
+            ctx = new ContextMenu();
+            exit = new MenuItem();
+
+            // Merge
+            ctx.MenuItems.AddRange(new MenuItem[] { exit });
+
+            // Let there be exit button
+            exit.Index = 0;
+            exit.Text = "Goodbye Kabul";
+            exit.Click += new System.EventHandler(this.exit_Click);
+
+            // Merge x2
+            kabulTray.ContextMenu = ctx;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Let it begin
+            kabulIdle = Resources.idle;
+            kabulPet = Resources.petting;
+
+            // Blanched Almond
+            this.BackColor = Color.BlanchedAlmond;
+            this.TransparencyKey = Color.BlanchedAlmond;
+
+            // Normel
+            kabulBox.Image = kabulIdle;
+        }
+
+        private void kabulBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            kabulBox.Image = kabulPet;
+        }
+
+        private void kabulBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            kabulBox.Image = kabulIdle;
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
